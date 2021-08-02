665324342debe9a6f909c27016e36c5e84f3fad8
Fernando Junior <fernando.junior85@outlook.com>
2021-08-02 16:08:49 -0300

    Complished

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Equipe = (props) => {
+class Equipe extends Component {
+  render () {
+    return (
+      <div>
+        <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} />
+        <hr />
+      </div>
+    )
+  }
+}
+
+class Sobre extends Component {
+  render () {
+    return (
+      <div>
+        <h2>Olá, sou o(a) { this.props.nome }</h2>
+        <h3>Cargo: { this.props.cargo }</h3>
+        <h3>Idade: { this.props.idade } anos</h3>
+      </div>
+    )
+  }
+}
+
+function App () {
   return (
     <div>
-      <Sobre nome={ props.nome } cargo={ props.cargo } idade={ props.idade } />
-      <Social facebook={ props.facebook } linkedin={ props.linkedin } youtube={ props.youtube }/>
-      <hr />
+      <h1>Conheça nossa equipe:</h1>
+      <Equipe nome="Fernando" cargo="Programador" idade="35" />
+      <Equipe nome="Lucas" cargo="Designer" idade="32" />
     </div>
   )
 }
 
-const Sobre = (props) => {
-  return (
-    <div>
-      <h2>Olá, sou o(a) { props.nome }</h2>
-      <h3>Cargo: { props.cargo }</h3>
-      <h3>Idade: { props.idade } anos</h3>
-    </div>
-  )
-}
-
-const Social = (props) => {
-  return (
-    <div>
-      <a href={ props.facebook }>Facebook </a>
-      <a href={ props.linkedin }>LinkedIn </a>
-      <a href={ props.youtube }>Youtube</a>
-    </div>
-  )
-}
-
-const App = () => {
-  return (
-    <>
-    <h1>Conheça nossa equipe:</h1>
-    <Equipe nome="Fernando" cargo="Programador" idade="35" facebook="https://facebook.com" linkedin="https://linkedin.com" youtube="https://youtube.com"/>
-    <Equipe nome="Lucas" cargo="Designer" idade="29" facebook="https://facebook.com" linkedin="https://linkedin.com" youtube="https://youtube.com" />
-    </>
-  )
-}
 export default App
