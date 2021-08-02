ee5a3346bbc355d4110c5b4edb05c47dd71ed97a
Fernando Junior <fernando.junior85@outlook.com>
2021-08-02 18:14:49 -0300

    Complished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
 
-import Membro from './components/Membro'
-
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      // status: 1
+      status: false
+    }
+
+    this.entrar = this.entrar.bind(this)
+    this.sair = this.sair.bind(this)
+  }
+
+  entrar () {
+    this.setState({ status: true })
+  }
+
+  sair () {
+    this.setState({ status: false })
+  }
   render () {
     return (
       <div>
-        <Membro nome="Visitante" />
+        {/* {
+          this.state.status === 1 && 
+          <h1>Bem-vindo ao sistema!</h1>
+        } */}
+
+        {
+          this.state.status
+            ? <div>
+                <h2>Bem-vindo ao sistema!</h2>
+                <button onClick={this.sair}>Sair do Sistema</button>
+              </div>
+
+            : <div>
+                <h2>Olá visitante, faça o login!</h2>
+                <button onClick={this.entrar}>Entrar no Sistema</button>
+              </div>
+        }
       </div>
     )
   }
