0c6d02bfe19794a49d9fe7dc8e3449cb982c90df
Fernando Junior <fernando.junior85@outlook.com>
2021-08-02 16:45:34 -0300

    Complished

--- src/App.js
+++ src/App.js
@@ -3,37 +3,26 @@ import React, { Component } from 'react'
 class App extends Component {
 
   constructor (props) {
-    super (props)
+    super(props)
     this.state = {
-      contador: 0
-    }
-
-    this.aumentar = this.aumentar.bind(this)
-    this.diminuir = this.diminuir.bind(this)
-  }
-
-  aumentar () {
-    let state = this.state
-    state.contador++
-    this.setState(state)
-  }
-
-  diminuir () {
-    let state = this.state
-    state.contador--
-    this.setState(state)
-
-    if (state.contador < 0 ) {
-      state.contador = 0
+      hora: '00:00:00'
     }
   }
+
+  componentDidMount () {
+    setInterval (() => {
+      this.setState({hora: new Date().toLocaleTimeString()})
+    }, 1000)
+  }
+
+  componentDidUpdate () {
+    console.log('Atualizou!')
+  }
+
   render () {
     return (
       <div>
-        <h1>Contador:</h1>
-        <h3>{ this.state.contador}</h3>
-        <button onClick={this.diminuir}>-</button>
-        <button onClick={this.aumentar}>+</button>
+        <h1>Meu Projeto { this.state.hora }</h1>
       </div>
     )
   }
