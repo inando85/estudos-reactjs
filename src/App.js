336c9c39f6cefbebb4ec7f1d34d8f8414b0de433
Fernando Junior <fernando.junior85@outlook.com>
2021-08-02 15:54:18 -0300

    Aulas de Props concluídas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Equipe = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sobre nome={ props.nome } cargo={ props.cargo } idade={ props.idade } />
+      <Social facebook={ props.facebook } linkedin={ props.linkedin } youtube={ props.youtube }/>
+      <hr />
     </div>
-  );
+  )
 }
 
-export default App;
+const Sobre = (props) => {
+  return (
+    <div>
+      <h2>Olá, sou o(a) { props.nome }</h2>
+      <h3>Cargo: { props.cargo }</h3>
+      <h3>Idade: { props.idade } anos</h3>
+    </div>
+  )
+}
+
+const Social = (props) => {
+  return (
+    <div>
+      <a href={ props.facebook }>Facebook </a>
+      <a href={ props.linkedin }>LinkedIn </a>
+      <a href={ props.youtube }>Youtube</a>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+    <h1>Conheça nossa equipe:</h1>
+    <Equipe nome="Fernando" cargo="Programador" idade="35" facebook="https://facebook.com" linkedin="https://linkedin.com" youtube="https://youtube.com"/>
+    <Equipe nome="Lucas" cargo="Designer" idade="29" facebook="https://facebook.com" linkedin="https://linkedin.com" youtube="https://youtube.com" />
+    </>
+  )
+}
+export default App
