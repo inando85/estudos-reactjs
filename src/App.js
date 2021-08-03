e677d56db99dfa26d8eb0c8069d75f1bf3c243a1
Fernando Junior <fernando.junior85@outlook.com>
2021-08-03 17:44:02 -0300

    Complished

--- src/App.js
+++ src/App.js
@@ -2,32 +2,21 @@ import React, { useState } from 'react'
 
 function App () {
 
-  const [tarefas, setTarefas] = useState([
-    'Pagar a conta de luz',
-    'Estudar React Hooks'
-  ])
+  const [contador, setContador] = useState(0)
 
-  const [input, setInput] = useState('')
-
-  const handleAdd = () => {
-    setTarefas([...tarefas, input])
-    setInput('')
+  const handleIncrement = () => {
+    setContador(contador + 1)
   }
-
+  const handleDecrement = () => {
+    setContador(contador - 1)
+  }
 
   return (
     <div>
       <h1>Hooks - useState</h1>
-
-      <ul>
-        {
-          tarefas.map(tarefa => (
-            <li key={tarefa}>{tarefa}</li>
-          ))
-        }
-      </ul>
-      <input type="text" value={input} onChange={(event) => setInput(event.target.value)}/>
-      <button type="button" onClick={handleAdd}>Adicionar</button>
+      <h4>{contador}</h4>
+      <button onClick={handleIncrement}>Adicionar</button>
+      <button onClick={handleDecrement}>Remover</button>
     </div>
   )
 }
